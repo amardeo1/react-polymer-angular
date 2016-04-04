331fb06f2826d0a0a30c7ca53bc5346827871206
Amar Deo <amar_deo41@hotmail.com>
2016-04-04 04:01:44 -0500

    added displayTime directive to show time elapsed after comment posted

--- angular/app/scripts/directives/comment/comment.js
+++ angular/app/scripts/directives/comment/comment.js
@@ -6,19 +6,23 @@
  * @description
  * # comment
  */
-angular.module('comment', [])
+angular.module('comment', ['msgTime'])
   .directive('commentModel', function () {
     return {
       template: '<div class="comment">' +
-                  '<h2 class="commentAuthor">' +
-                      '{{author}}' +
-                  '</h2>' +
+				  '<div>' +
+					  '<h2 class="commentAuthor">' +
+						  '{{author}}' +
+					  '</h2>' +
+					  '<display-time class="timeMsg" time="{{msgPostedTime}}"></display-time>' +
+				  '</div>' +
                   '<ng-transclude></ng-transclude>' +
                 '</div>',
       restrict: 'E',
       transclude: true,
       scope: {
-        author: '@'
+        author: '@',
+		msgPostedTime: '@'
       },
       link: function postLink(scope, element, attrs) {}
     };
